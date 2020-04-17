0ad5bc5a839094c73763a2c734b88818e6b3b9d1
dmitry10x <fesfdi@gmail.com>
2020-04-17 17:37:12 +0300

    os

---
+++ os.py
@@ -0,0 +1,37 @@
+import os
+
+#os.getcwd() --> pwd
+#os.chdir
+#os.listdir() --> ls
+#os.mkdir('folder_name')
+#os.makekdirs('folder_name/sub_dir')
+
+#os.rmdir('folder_name')
+#os.removedirs('folder_name/sub_dir')
+
+#os.rename('old_file_name.txt', 'new_file_name.txt')
+#os.stat('file_name')
+#os.stat('file_name').st_size --> file size in bytes
+
+#os.walk()
+
+
+# print(os.getcwd())
+# os.chdir('C:/Users/Дмитрий/')
+# print(os.getcwd())
+# os.chdir('C:/Users/Дмитрий/Desktop')
+# print(os.listdir())
+
+
+# for dirpath, dirnames, filenames in os.walk('C:/Users/Дмитрий/'):
+#     print('Current Path:', dirpath)
+#     print('Directories:', dirnames)
+#     print('Files', filenames)
+
+
+# print(os.environ)
+print(os.environ.get('HOME'))
+print(os.environ.get('OS'))
+
+print(os.environ.get('HOME'))
+
