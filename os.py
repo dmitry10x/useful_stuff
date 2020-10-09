ae79133d0663571ab9a6a92b631e0a0541ff8a45
dmitry10x <fesfdi@gmail.com>
2020-10-09 17:36:09 +0300

    os

--- os.py
+++ os.py
@@ -2,14 +2,16 @@ import os
 
 #os.getcwd() --> pwd
 #os.chdir
-#os.listdir() --> ls
+#os.listdir() --> ls / get list of file_names located in the directory
 #os.mkdir('folder_name')
 #os.makekdirs('folder_name/sub_dir')
 
 #os.rmdir('folder_name')
 #os.removedirs('folder_name/sub_dir')
 
-#os.rename('old_file_name.txt', 'new_file_name.txt')
+#os.rename('old_file_name.txt', 'new_file_name.txt') #on windows may not work properly, instead try os.replace()
+os.replace(r'path/old_name',r'path/new_name')
+
 #os.stat('file_name')
 #os.stat('file_name').st_size --> file size in bytes
 
